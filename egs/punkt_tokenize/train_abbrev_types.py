c99d1ba1ca162e7e560ebb4dd85e9058d52a860c
Vu Anh <anhv.ict91@gmail.com>
2018-12-13 12:47:30 +0700

    add abbrev types

--- egs/punkt_tokenize/train_abbrev_types.py
+++ egs/punkt_tokenize/train_abbrev_types.py
@@ -4,7 +4,7 @@ from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktTrainer
 trainer = PunktTrainer()
 trainer.INCLUDE_ALL_COLLOCS = True
 # Corpus: One sentence per line
-with open('./corpus/corpus_test.txt', 'r') as fs:
+with open('data/test.txt', 'r') as fs:
     text = fs.read()
 rs = trainer.train(text)
 abbrev_types = trainer.get_params().abbrev_types
