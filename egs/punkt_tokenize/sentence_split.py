878a8cf72045afc6f3f22e081ae1008f3c1843b1
Vu Anh <anhv.ict91@gmail.com>
2018-12-21 13:43:51 +0700

    add test

--- egs/punkt_tokenize/sentence_split.py
+++ egs/punkt_tokenize/sentence_split.py
@@ -11,6 +11,9 @@ punkt_param.abbrev_types = {'g.m.t', 'e.g', 'dr', 'dr', 'vs', "000", 'mr', 'mrs'
 sentence_splitter = PunktSentenceTokenizer(punkt_param)
 if __name__ == '__main__':
     text = 'Theo người nhà nạn nhân, T. bị dập não trước, gẫy lìa đùi trái, dập chân phải và bị đa chấn thương toàn thân. Hiện T. vẫn lúc tỉnh, lúc mê nên các bác sĩ vẫn chưa thể tiến hành phẫu thuật.'
+    text = 'Vào giữa những năm 1990, BS. Diệp Giản Minh, sinh năm 1977, có công kiem duoc $55.000.000 việc đơn giản trong một khu rừng.'
+    text = 'Trong những ngày qua, khi “Mệnh lệnh 02” của Giám đốc Công an TP Hà Nội về việc lập lại trật tự giao thông đô thị đã nhận được sự đồng tình ủng hộ của đa số người dân thủ đô. Hàng nghìn trường hợp vi phạm trật tự giao thông đô thị đã được cơ quan chức năng trên toàn địa bàn TP Hà Nội xử lí. Tuy nhiên một số bất cập đã được lực lượng Công an thống kê và đề xuất với cơ quan quản lí Nhà nước.'
+    text = 'The University of Chicago Press là một địa chỉ uy tín để các độc giả tìm đọc về Hayek.Dự án Collected Works of F. A. Hayek của họ đã cho xuất bản 19 tác phẩm và một quyển tự truyện của Hayek với sự biên tập kỹ lưỡng và tổng hợp thêm các bài phỏng vấn, bài báo nghiên cứu và thư tay, và một số bản thảo chưa được công bố của Hayek.'
     for sent in sentence_splitter.tokenize(text):
         print(sent)
 
